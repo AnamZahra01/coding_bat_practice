ea9a17a74c35b6638ff1c81e82bf48267274fb77
Anam Zahra <zanam090@gmail.com>
2022-08-23 17:08:27 -0400

    Array-1

---
+++ src/array_1/MakePi.java
@@ -0,0 +1,14 @@
+package array_1;
+/*
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+            makePi() → [3, 1, 4]
+ */
+public class MakePi {
+
+    public int[] makePi() {
+        int[] value = {3, 1, 4};
+        return value;
+    }
+
+}
+
