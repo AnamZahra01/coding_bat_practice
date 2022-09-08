bf49f446380345adc0f2473deb6527ece15da481
Anam Zahra <zanam090@gmail.com>
2022-09-08 18:46:42 -0400

    Array-1

--- src/array_1/MakeMiddle.java
+++ src/array_1/MakeMiddle.java
@@ -7,7 +7,7 @@ Given an array of ints of even length, return a new array length 2 containing th
  */
 public class MakeMiddle {
 
-    public int[] makeMiddle(int[] nums) {
+    public int[] makeMiddle(int[] nums){
         int [] arr = new int [2];
         if(nums.length == 2){
             return nums;
