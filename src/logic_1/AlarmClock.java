f53ec460ec01454c269735d98f7580aa2c5c444a
Anam Zahra <zanam090@gmail.com>
2022-09-13 19:13:09 -0400

    Logic-1

---
+++ src/logic_1/AlarmClock.java
@@ -0,0 +1,21 @@
+package logic_1;
+/*
+Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation, return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the alarm should be "7:00" and on the weekend it should be "10:00". Unless we are on vacation -- then on weekdays it should be "10:00" and weekends it should be "off".
+            alarmClock(1, false) → "7:00"
+            alarmClock(5, false) → "7:00"
+            alarmClock(0, false) → "10:00"
+ */
+public class AlarmClock {
+
+    public String alarmClock(int day, boolean vacation) {
+        boolean weekday = day >= 1 && day <= 5;
+        boolean off = day == 0 || day == 6;
+        if(weekday && vacation == false){
+            return "7:00";
+        }else if((weekday && vacation == true) || (off && vacation == false)){
+            return "10:00";
+        }
+        return "off" ;
+    }
+
+}
