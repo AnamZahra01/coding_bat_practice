87f25c41d4f7ceb272c9287337a312ac98545a81
Anam Zahra <zanam090@gmail.com>
2022-09-13 20:29:28 -0400

    Array-1

---
+++ src/array_1/MaxTriple.java
@@ -0,0 +1,19 @@
+package array_1;
+/*
+Given an array of ints of odd length, look at the first, last, and middle values in the array and return the largest. The array length will be a least 1.
+            maxTriple([1, 2, 3]) → 3
+            maxTriple([1, 5, 3]) → 5
+            maxTriple([5, 2, 3]) → 5
+ */
+public class MaxTriple {
+
+    public int maxTriple(int[] nums) {
+        if(nums[0] > nums[nums.length -1] && nums[0] > nums[nums.length/2]){
+            return nums[0];
+        }else if(nums[nums.length/2] > nums[nums.length -1] && nums[nums.length/2] > nums[0]){
+            return nums[nums.length/2];
+        }
+        return nums[nums.length -1];
+    }
+
+}
